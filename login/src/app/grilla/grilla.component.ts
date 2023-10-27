4eb772f66dbfcfb43ecba227945c8849a913745e
Informatica Electoral <soporte@informaticaelectoral.com>
2023-10-27 12:18:46 -0700

    interfaz

--- login/src/app/grilla/grilla.component.ts
+++ login/src/app/grilla/grilla.component.ts
@@ -10,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class GrillaComponent {
   form: FormGroup;
   selectedFile: File | null = null;
+  imageUrl: string = '';
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.form = this.fb.group({
@@ -20,16 +21,19 @@ export class GrillaComponent {
     const fileInput = event.target;
     this.selectedFile = fileInput.files[0];
   }
-  
   uploadImage() {
     if (this.selectedFile) {
       const formData = new FormData();
+      let path
       formData.append('image', this.selectedFile);
       console.log(typeof formData)
       //Enviar el nombre como objeto JSON
       this.http.post('http://0.0.0.0:8100/recibir-imagen',formData).subscribe(
         (response) => {
-          console.log('Respuesta del servidor:',response);
+
+          this.imageUrl = `"Backend-login/${response}"`;
+          console.log('Respuesta del servidor:',this.imageUrl);
+
           //Realiza acciones adicionales después de enviar los datos, si es necesario
         },
         (error) => {
