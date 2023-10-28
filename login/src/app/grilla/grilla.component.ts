63375c6c3cf17556cf7295c0ff2eb80f7aa6eb5d
Informatica Electoral <soporte@informaticaelectoral.com>
2023-10-28 11:25:04 -0700

    url

--- login/src/app/grilla/grilla.component.ts
+++ login/src/app/grilla/grilla.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-grilla',
@@ -11,10 +12,11 @@ export class GrillaComponent {
   form: FormGroup;
   selectedFile: File | null = null;
   imageUrl: string = '';
+  mostrarDiv: boolean = false;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient,private sanitizer: DomSanitizer) {
     this.form = this.fb.group({
-      fileInput: [null, Validators.required]
+      fileInput: [null, Validators.required],
     });
   }
   onFileSelected(event: any) {
@@ -24,17 +26,23 @@ export class GrillaComponent {
   uploadImage() {
     if (this.selectedFile) {
       const formData = new FormData();
-      let path
+
       formData.append('image', this.selectedFile);
-      console.log(typeof formData)
       //Enviar el nombre como objeto JSON
       this.http.post('http://0.0.0.0:8100/recibir-imagen',formData).subscribe(
         (response) => {
 
-          this.imageUrl = `"Backend-login/${response}"`;
-          console.log('Respuesta del servidor:',this.imageUrl);
+          const url  = `http://10.112.32.178:8100/static/${response}`;
+          let imageUrl = this.sanitizer.bypassSecurityTrustUrl(url);
+          console.log('datos:',imageUrl)
+          const valores = Object.values(imageUrl);
 
-          //Realiza acciones adicionales después de enviar los datos, si es necesario
+          this.imageUrl = valores[0]
+
+          console.log('type',typeof imageUrl)
+          console.log('Respuesta del servidor:',imageUrl);
+          this.mostrarDiv = true;
+
         },
         (error) => {
           console.error('Error al enviar datos:', error);
@@ -42,4 +50,5 @@ export class GrillaComponent {
       );
     };
   }
+
 }
